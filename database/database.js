988d0b828737fdcb719115d73e1e80bdedc444ad
M K SUPREYA <supreya1447@gmail.com>
2026-08-22 15:31:15 +0530

    Delete database directory

--- database/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-// Returns simple connection status info, used for health checks if needed
-const getConnectionStatus = () => {
-  const states = ['disconnected', 'connected', 'connecting', 'disconnecting'];
-  return states[mongoose.connection.readyState] || 'unknown';
-};
-
-module.exports = { getConnectionStatus };
